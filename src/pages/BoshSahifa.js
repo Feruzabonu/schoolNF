0c0053e59a82548df4c37d246a3cb713693ab57e
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-09-29 16:04:36 +0500

    r

--- src/pages/BoshSahifa.js
+++ src/pages/BoshSahifa.js
@@ -113,7 +113,7 @@ export default class BoshSahifa extends Component {
     return (
       <div>
         {this.state.loader ? (
-          <div className="loaderT">
+          <div className="loader">
             <ScaleLoader color="#1EB2A6" loading={this.state.loader} size={120} />
           </div>
         ) : (
