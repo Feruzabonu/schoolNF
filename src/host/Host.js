f839c0a6a080c7ee3c70298b72ea4633c3239ccc
Feruzabonu <feruzabonu12@gmail.com>
2021-09-29 15:31:23 +0500

    f

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const url = "http://143.244.209.138";
-export const idMaktab = "2";
-export const user = "5";
+export const idMaktab = "3";
+export const user = "6";
 export const httpRequest = (config) => {
   return axios({
     ...config,
