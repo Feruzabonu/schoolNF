5be02cd41e02a6cba6b1625d9fe1a97fe09b2ada
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-09-30 16:27:20 +0500

    fdvfe

--- src/pages/BoshSahifa.js
+++ src/pages/BoshSahifa.js
@@ -7,6 +7,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import React, { Component } from "react";
 import style from "./BoshSahifa.module.css";
+import pic20 from "../img/twitter 2.png";
+
 import { Button, Container, Nav,Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import rasm1 from "../img/vasily-koloda-8CqDvPuo_kI-unsplash.jpg";
@@ -284,6 +286,48 @@ export default class BoshSahifa extends Component {
                 </div>
               </div>
             </div>
+            <div className={style.elem}>
+              <Row style={{ alignItems: "center" }}>
+                <Col className={style.htr} lg={2} md={12} sm={12}>
+                  <h2 className={style.colT}>
+                    Bizning ijtimoiy sahifalarimiz:
+                  </h2>
+                </Col>
+
+                <Col className={style.htr} lg={2} md={6} sm={12}>
+                  <a src="#" className={style.colT} target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Telegram_Messenger.png" />
+                    <p>Telegram sahifasi</p>
+                  </a>
+                </Col>
+                <Col className={style.htr} lg={2} md={6} sm={12}>
+                  <a src="#" className={style.colT} target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" />
+                    <p>Instagram sahifasi</p>
+                  </a>
+                </Col>
+                <Col className={style.htr} lg={2} md={6} sm={12}>
+                  <a src="#" className={style.colT} target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" />
+                    <p>Facebook sahifasi</p>
+                  </a>
+                </Col>
+                <Col className={style.htr} lg={2} md={6} sm={12}>
+                  <a src="#" className={style.colT} target="_blank">
+                    <img src={pic20} />
+                    <p>Twitter sahifasi</p>
+                  </a>
+                </Col>
+                <Col className={style.htr} lg={2} md={6} sm={12}>
+                  <a src="#" className={style.colT} target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png" />
+                    <p>You tube sahifasi</p>
+                  </a>
+                </Col>
+              </Row>
+            </div>
+            <br />
+            <br />
             <MaktabTadbirlari />
             <Footer />
           </div>
